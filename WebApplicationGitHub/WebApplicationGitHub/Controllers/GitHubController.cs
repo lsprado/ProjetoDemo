87e115fc70efdb706c312f532f49b98f347bbc3b
Leandro Silveira Prado <leandro.prado@outlook.com.br>
2015-08-19 18:13:32 -0300

    adicionado o controller

---
+++ WebApplicationGitHub/WebApplicationGitHub/Controllers/GitHubController.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplicationGitHub.Controllers
+{
+    public class GitHubController : Controller
+    {
+        // GET: GitHub
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: GitHub/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: GitHub/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: GitHub/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: GitHub/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: GitHub/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: GitHub/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: GitHub/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
